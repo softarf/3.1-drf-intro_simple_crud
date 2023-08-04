b59e45ea9bec6b9b825cfef01cc665d0d749c220
softarf <98207887+softarf@users.noreply.github.com>
2023-08-04 11:16:32 +0500

    Уточняет правила хранения данных в моделе Project.
    Задаёт требование уникальности для названий термодатчиков
    и требование уникальности для сочетаний координат широты и долготы.
    
    Changes committed:
       1. modified:   measurements/models.py

--- measurements/models.py
+++ measurements/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     """ 'Датчик' - Прибор, производящий измерения на объекте. """
 
-    name = models.TextField(verbose_name='Название')         # location or model & inventory number
+    name = models.TextField(verbose_name='Название', unique=True)         # location or model & inventory number
     latitude = models.FloatField(verbose_name='Широта')      # -90 < x <= 90
     longitude = models.FloatField(verbose_name='Долгота')    # -180 < x <= 180
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
@@ -16,6 +16,7 @@ class Project(models.Model):
         verbose_name = 'Термодатчик'
         verbose_name_plural = 'Термодатчики'
         ordering = ['id']
+        unique_together = ('latitude', 'longitude')
 
     def __str__(self):
         return self.name
