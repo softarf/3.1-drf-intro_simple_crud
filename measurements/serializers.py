f48b9836e3b43d614ced791ccbc96186bd2dc908
softarf <98207887+softarf@users.noreply.github.com>
2023-08-04 16:05:35 +0500

    Создаёт сериализаторы для конвертации моделей.
    Changes committed:
        1. modified:   measurements/serializers.py

--- measurements/serializers.py
+++ measurements/serializers.py
@@ -1,1 +1,32 @@
-# TODO: опишите сериализаторы
+from rest_framework import serializers
+
+from measurements.models import Measurement, Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'latitude', 'longitude', 'created_at', 'updated_at']    # Отображаемые поля.
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Measurement
+        fields = ['id', 'project', 'value', 'created_at']    # Отображаемые поля.
+
+
+class NestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['id', 'value', 'created_at']    # Отображаемые поля.
+
+
+class ProjectDetailSerializer(serializers.ModelSerializer):
+
+    measurements = NestedSerializer(read_only=True, many=True, required=False)  # Maybe optionally measurements.
+
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'latitude', 'longitude', 'created_at', 'updated_at', 'measurements']
